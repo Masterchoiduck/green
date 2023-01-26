522c2e55508e60e4eb828bf30efe004b5f8dfec9
5세훈 <1>
2023-01-26 19:50:19 +0900

    2023.01.26 나는 바보 오세훈 데헷

---
+++ 5세훈/Final Project/js/universalTemaTyping.js
@@ -0,0 +1,19 @@
+"use strict";
+const content = "Universal Studios KOREA 테마."
+const text = document.querySelector(".main_tema_body_text")
+let index = 0;
+
+ function sleep(delay){ 
+     const start = new Date().getTime(); 
+     while (new Date().getTime() < start + delay); 
+    }
+
+function typing(){
+    text.textContent += content[index++];
+    if(index > content.length){
+        text.textContent = ""
+        index = 0;
+        sleep(3000);
+    }
+}
+setInterval(typing, 300)
