aae0d1c3457763e16acdeb36fd48c4d6a4ecb9b0
최성환 <csh98419@naver.com>
2023-01-30 17:31:38 +0900

    2023.01.30 1731부

--- 현주/main.js
+++ 현주/main.js
@@ -1,9 +1,40 @@
+const $header = document.querySelector('header')
+const $menu_bar =document.querySelectorAll('.menu_bar')
+const $search_icon_gray=document.querySelector('.search_icon_gray')
+const $search_icon_white=document.querySelector('.search_icon_white')
+const $ticket_icon_white=document.querySelector('.ticket_icon_white')
+const $ticket_icon_gray=document.querySelector('.ticket_icon_gray')
+const $user_icon_white=document.querySelector('.user_icon_white')
+const $user_icon_gray=document.querySelector('.user_icon_gray')
+const $logo_white=document.querySelector('.logo_white')
+const $logo_gray=document.querySelector('.logo_gray')
+
 window.addEventListener('scroll', (e) => { 
     // console.log(window.scrollY);
-    if (window.scrollY>100) {
-      console.log('변하기');
-    }
-    if (window.scrollY<100) {
-      console.log('안변하기');
-    }
+    $menu_bar.forEach(item=>{
+      // 검정
+      if (window.scrollY>890) {
+        item.classList.add('csh__black')
+        $search_icon_gray.style.display='inline-block'
+        $search_icon_white.style.display='none'
+        $ticket_icon_gray.style.display='inline-block'
+        $ticket_icon_white.style.display='none'
+        $user_icon_gray.style.display='inline-block'
+        $user_icon_white.style.display='none'
+        $logo_gray.style.display='inline-block'
+        $logo_white.style.display='none'
+      }
+      // 흰색
+      if (window.scrollY<890) {
+        item.classList.remove('csh__black')
+        $search_icon_gray.style.display='none'
+        $search_icon_white.style.display='inline-block'
+        $ticket_icon_gray.style.display='none'
+        $ticket_icon_white.style.display='inline-block'
+        $user_icon_gray.style.display='none'
+        $user_icon_white.style.display='inline-block'
+        $logo_gray.style.display='none'
+        $logo_white.style.display='inline-block'
+      }
+    })
   });
