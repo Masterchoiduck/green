9f2ff8b9b72f745838b01082f24adb83964b67db
csh <csh98419@naver.com>
2023-02-02 01:49:36 +0900

    현듀빼액 보아라

--- 현주/test3_.js
+++ 현주/test3_.js
@@ -59,62 +59,22 @@
 //어트랙션 호버 - 이미지 변경
 
 
-const blurImg1 = document.querySelectorAll('.att_s');
-const blurImg2 = document.querySelector('.att_s2');
-const blurImg3 = document.querySelector('.att_s3');
-const blurImg4 = document.querySelector('.att_s4');
-const blurImg5 = document.querySelector('.att_s5');
-const blurImg6 = document.querySelector('.att_s6');
-
-// const attTitle = document.querySelector('.att_box');
-/* 1 */
-// blurImg1.addEventListener('mouseover', (event)=> {
-//   attTitle.style.display = "block";
-//   blurImg1.style.backgroundImage ="url('./img/web/어트_해리포터 앤드 포비든 저니 블러.png')";
-// })
-// blurImg1.addEventListener('mouseout', (event)=> {
-//   attTitle.style.display = "none";
-//   blurImg1.style.backgroundImage ="url('./img/web/어트_해리포터 앤드 포비든 저니.png')";
-// })
-/* 2 */
-// blurImg2.addEventListener('mouseover', (event)=> {
-//   attTitle.style.display = "block";
-//   blurImg2.style.backgroundImage ="url('./img/web/어트_쥬라기월드 더 라이드 블러.png')";
-// })
-// blurImg2.addEventListener('mouseout', (event)=> {
-//   attTitle.style.display = "none";
-//   blurImg2.style.backgroundImage ="url('./img/web/어트_쥬라기월드 더 라이드.png')";
-// })
 
 
 
 const $att_s=document.querySelectorAll('.att_s')
-const $att_box = document.querySelectorAll('.att_box')
 const $att_title = document.querySelectorAll('.att_title')
-// $att_s.forEach(item1=>{
-//   $att_box.forEach(item2=>{
-//     console.log(item1);
-//     item2.addEventListener('mouseover',e=>{
-//       item2.style.display = "none"
-//       item2.style.backgroundImage ="url('./img/web/어트_해리포터 앤드 포비든 저니 블러.png')";
-//     })
-//     item1.addEventListener('mouseout',e=>{
-//       item1.style.display = "block"
-//     })
-//   })
-// })
 const test_title1 =[]
 const test_title2 = []
 $att_title.forEach(item=>{
   test_title1.push(item.childNodes[1].textContent)
   test_title2.push(item.childNodes[3].textContent)
-  if(item.childNodes[3].textContent==='ㅇㅇㅇㅇㅇ'){
-    console.log('찾았다');
-    console.log(item.childNodes[3].textContent);
-  }
 })
 
 function test(num,aa){
+  if(aa===3){
+    return `url('./img/web/어트_${test_title1[num]} ${test_title2[num]} Big.png')`
+  }
   if(aa){
     return `url('./img/web/어트_${test_title1[num]} ${test_title2[num]} 블러.png')`
   } else {
@@ -122,18 +82,20 @@ function test(num,aa){
   }
 }
 
-console.log(test2());
-// console.log(test(1));
-console.log("이거야됨 : url('./img/web/어트_쥬라기월드 더 라이드 블러.png')");
+const att_b1 = document.querySelector('.att_b1')
 $att_s.forEach((item,index)=>{
 
     const att_box = item.querySelector('.att_box');
     item.addEventListener('mouseover', e => {
+      item.style.backgroundImage =test(index,1);
       att_box.classList.remove('d-none');
+      att_b1.style.backgroundImage = test(index,3)
     });
 
     item.addEventListener('mouseout', e => {
+      item.style.backgroundImage =test(index,0);
       att_box.classList.add('d-none');
+      att_b1.style.backgroundImage = 'none'
     })
 })
         
