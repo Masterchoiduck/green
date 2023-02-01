7a20d2af91238f7c8daf9b2c6bb787d8ace2935c
현듀빼액 <selly1996@naver.com>
2023-02-01 17:01:36 +0900

    2023.2.1 임시...

--- 현주/test3.js
+++ 현주/test3.js
@@ -54,3 +54,40 @@
               ]
         });
       });
+
+
+//어트랙션 호버 - 이미지 변경
+
+
+const blurImg1 = document.querySelector('.att_s1');
+const blurImg2 = document.querySelector('.att_s2');
+const blurImg3 = document.querySelector('.att_s3');
+const blurImg4 = document.querySelector('.att_s4');
+const blurImg5 = document.querySelector('.att_s5');
+const blurImg6 = document.querySelector('.att_s6');
+
+const attTitle = document.querySelector('.att_box');
+/* 1 */
+blurImg1.addEventListener('mouseover', (event)=> {
+  attTitle.style.display = "block";
+  blurImg1.style.backgroundImage ="url('./img/web/어트_해리포터 앤드 포비든 저니 블러.png')";
+})
+blurImg1.addEventListener('mouseout', (event)=> {
+  attTitle.style.display = "none";
+  blurImg1.style.backgroundImage ="url('./img/web/어트_해리포터 앤드 포비든 저니.png')";
+})
+/* 2 */
+blurImg2.addEventListener('mouseover', (event)=> {
+  attTitle.style.display = "block";
+  blurImg2.style.backgroundImage ="url('./img/web/어트_쥬라기월드 더 라이드 블러.png')";
+})
+blurImg2.addEventListener('mouseout', (event)=> {
+  attTitle.style.display = "none";
+  blurImg2.style.backgroundImage ="url('./img/web/어트_쥬라기월드 더 라이드.png')";
+})
+/*
+attTitle.addEventListener('mouseover', (event)=> {
+  attTitle.style.display="none";
+})
+
+*/
