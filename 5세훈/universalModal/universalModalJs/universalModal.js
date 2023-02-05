6958fa5cd3b9b8579c23f37364a5bd1fa90ac8d5
오세훈 <wbsldjgktka@naver.com>
2023-02-05 17:38:12 +0900

    모달달달달

--- 5세훈/universalModal/universalModalJs/universalModal.js
+++ 5세훈/universalModal/universalModalJs/universalModal.js
@@ -2,6 +2,30 @@ const openButton = document.getElementById("open");
 const modal = document.querySelector(".modal");
 const overlay = modal.querySelector(".md_overlay");
 const closeButton = modal.querySelector("button");
+
+
+const $detailBox = document.querySelector('.detail_commentbox');
+const $aboutBox = document.querySelector('.aboutbox');
+const $detailBtn = document.querySelector('.toggle_header');
+const $aboutBtn = document.querySelector('.toggle_header2');
+//detail about 버튼
+$aboutBtn.addEventListener('click',e=> { 
+    $detailBox.style.display = 'none';
+    $aboutBox.style.display = 'block';
+    $detailBtn.style.color = '#ccc';
+    $aboutBtn.style.color = '#000';
+});
+
+$detailBtn.addEventListener('click',e=> { 
+    $detailBox.style.display = 'block';
+    $aboutBox.style.display = 'none';
+
+    $detailBtn.style.color = '#000';
+    $aboutBtn.style.color = '#ccc';
+});
+
+
+
 //동작함수
 const openModal = () => {
     modal.classList.remove("hidden");
